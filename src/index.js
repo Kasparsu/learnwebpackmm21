5b7f274f43537293fb6a2b8e5fe4e5cca2632897
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-09-20 12:38:50 +0300

    add module example

--- src/index.js
+++ src/index.js
@@ -1,46 +1,5 @@
-
 import './style.scss';
 
+import hello from './hello.js';
 
-let name = 'kaspar';
-console.log(name);
-const pi = 3.14;
-
-let obj = {
-    name: 'asd',
-    age: 21,
-    pets: [
-        {
-            name: 'asd',
-        }
-    ],
-    hello1(){
-        //do something
-    },
-    hello2: function(){
-        //do something
-    },
-    hello3: () => {
-
-    }
-};
-
-let nums = [0,3,2,1,4,5,6,7,8,9];
-
-nums.forEach(function(value){
-    console.log(value);
-});
-nums.forEach(value => console.log(value));
-
-let powers = nums.map(value => value*value);
-console.log(powers);
-let sum = nums.reduce((prev, cur) => prev+cur);
-console.log(sum);
-let even = nums.filter(value => value%2==0);
-console.log(even);
-let sort = nums.sort();
-console.log(sort);
-let find = nums.findIndex(value => {
-    return value == 4;
-});
-console.log(find);
+hello.hello();
