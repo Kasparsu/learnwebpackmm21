9e4dedf92dd67babb56749467850d33a88ad6cfe
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-09-21 14:10:03 +0300

    add items list example

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,74 @@
 <template>
-    
-    <input type="text" v-model="name">
-    <button class="button" @click="items.push(name)">Add</button>
-    <ul>
-        <li v-for="(item,index) in items" :key="index">{{ item }}</li>
-    </ul>
+    <div class="container">
+        <section class="section">
+            <div class="field has-addons"> 
+                <p class="control">
+                    <input class="input" type="text" v-model="newItem" @keydown.enter="addItem()">
+                </p>
+                <p class="control">
+                    <button class="button" @click="addItem()">Add</button>        
+                </p>    
+            </div>
+            <div class="content">
+                <h1>All items</h1>
+                <ul>
+                    <li v-for="item in items" :key="item.id">
+                        {{ item.name }}
+                        <input type="checkbox" v-model="item.done">
+                    </li>
+                </ul>
+
+                <h1>Done items</h1>
+                <ul>
+                    <li v-for="item in doneItems" :key="item.id">
+                        {{ item.name }}
+                        <input type="checkbox" v-model="item.done">
+                    </li>
+                </ul>
+
+                <h1>Not Done items</h1>
+                <ul>
+                    <li v-for="item in notDoneItems" :key="item.id">
+                        {{ item.name }}
+                        <input type="checkbox" v-model="item.done">
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
 export default {
     data(){
         return {
-            name: 'Kaspar Martin Suursalu',
+            newItem: '',
             count: 0,
-            items: ['piim', 'sai', 'viin', 'õlu']
+            items: [
+                {name:'piim', done: true, id: 0},
+                {name:'sai', done: false, id: 1},
+                {name:'viin', done: true, id: 2},
+                {name:'õlu', done: false, id: 3},
+            ]
         }
+    },
+    methods: {
+        addItem(){
+            if(this.newItem.trim() != ''){
+                this.items.push({name:this.newItem, done: false, id: this.items.length});
+            }
+            this.newItem = '';
+        }
+    },
+    computed: {
+        doneItems(){
+            return this.items.filter(item => item.done);
+        },
+        notDoneItems(){
+            return this.items.filter(item => !item.done);
+        },
     }
+
 }
 </script>
 
