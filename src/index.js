dff48f78322d041c79917cfd59c203b8d2ded9bf
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-09-27 10:12:16 +0300

    Add components and vue-router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import './style.scss';
 
 import { createApp } from 'vue';
 
-import App from './App.vue';
+import * as VueRouter from 'vue-router';
 
-createApp(App).mount('#app');
+import App from './App.vue';
+import ToDoList from './ToDoList.vue';
+
+const routes = [
+    { path: '/', component: ToDoList }
+]
+
+const router = VueRouter.createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: VueRouter.createWebHashHistory(),
+    routes, // short for `routes: routes`
+})
+
+
+let app = createApp(App);
+app.use(router);
+
+app.mount('#app');
