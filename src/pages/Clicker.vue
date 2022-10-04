47fb98166d2c4ae89931cd3f26244f4431b8a163
Kaspar Keskyla <Kaspar.Keskyla2@tptlice.ee>
2022-10-04 14:12:43 +0300

    Add Cookie cost per purtage

--- src/pages/Clicker.vue
+++ src/pages/Clicker.vue
@@ -1,35 +1,24 @@
 <template>
-  <div class="columns">
-    <div class="column is-one-third has-text-centered">
-        <b>{{displayCookies}} cookies</b>
-        <br>
-        <b>{{displayCps}} cookies per second</b>
-        <img @click="cookieClick()" :class="{'click': clickClass}" src="https://www.picng.com/upload/cookie/png_cookie_11811.png">
+    <div class="columns">
+        <div class="column is-one-third has-text-centered">
+            <b>{{displayCookies}} Cookies</b><br>
+            <b>{{displayCps}} CPS (cookies per second)</b><br>
+            <b>{{displayClick}} CPC (cookies per click)</b>
+            <img @click="cookiesClick()" :class="{'click': clickClass}" src="https://www.picng.com/upload/cookie/png_cookie_11811.png" alt="cookie">
+        </div>
+        <div class="column">
+        <button @click="Upgrade(0.1, Cps1Cost)" class="button is-primary" :disabled="cookies<Cps1Cost">Buy 0.1 cps for {{Cps1Cost}} cookies</button>
+        <button @click="UpgradeClick(1, Cpc1Cost)" class="button is-primary" :disabled="cookies<Cpc1Cost">Buy +1 cookies per every click for {{Cpc1Cost}} cookies</button><br>
+        </div>
     </div>
-    <div class="column">
-        <button @click="upgrade(0.1, 10)" class="button is-primary" :disabled="cookies<10">Buy 0.1 cps for 10 cookies</button>
-        <button @click="upgrade(1, 100)" class="button is-primary" :disabled="cookies<100">Buy 1 cps for 100 cookies</button>
-        <button @click="upgrade(10, 1000)" class="button is-primary" :disabled="cookies<1000">Buy 10 cps for 1000 cookies</button>
-        <button @click="upgrade(100, 10000)" class="button is-primary" :disabled="cookies<10000">Buy 100 cps for 10000 cookies</button>
-        <button @click="upgrade(1000, 100000)" class="button is-primary" :disabled="cookies<100000">Buy 1000 cps for 100000 cookies</button>
-    </div>
-  </div>
 </template>
- 
- 
+
 <script>
 export default {
     created(){
-        setInterval(()=>{
+        setInterval(()=> {
             this.cookies += this.cps;
         }, 1000);
-    },
-    data(){
-        return {
-            clickClass: false,
-            cookies: 0,
-            cps: 0
-        }
     },
     computed: {
         displayCookies(){
@@ -37,22 +26,43 @@ export default {
         },
         displayCps(){
             return parseFloat(this.cps.toFixed(1));
+        },
+        displayClick(){
+            return parseFloat(this.click.toFixed(1));
+        },
+    },
+    data(){
+        return{
+            clickClass: false,
+            cookies: 0,
+            cps: 0,
+            click: 1,
+            Cpc1Cost: 10,
+            Cps1Cost: 10,
         }
     },
     methods: {
-        cookieClick(){
-            this.cookies++;
+        cookiesClick(){
+            this.cookies += this.click;
             this.clickClass = true;
             setTimeout(()=> {
                 this.clickClass = false;
             }, 100);
         },
-        upgrade(cps, cost){
-            if(this.cookies >= cost){
+        Upgrade(cps, cost){
+            if (this.cookies >= cost) {
                 this.cookies -= cost;
                 this.cps += cps;
+                this.Cps1Cost++;
             }
-        }
+        },
+        UpgradeClick(click, cost){
+            if (this.cookies >= cost) {
+                this.cookies -= cost;
+                this.click += click;
+                this.Cpc1Cost++;
+            }
+        },
     }
 }
 </script>
