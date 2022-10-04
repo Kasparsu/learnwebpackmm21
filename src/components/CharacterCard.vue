a8c001ddd6829a2c3985f79f9c4ebfb010a61b22
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-10-04 14:13:42 +0300

    add pagination examples

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -1,40 +1,31 @@
 <template>
-  <div class="card">
-  <div class="card-image">
-    <figure class="image is-4by3">
-      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-    </figure>
-  </div>
-  <div class="card-content">
-    <div class="media">
-      <div class="media-left">
-        <figure class="image is-48x48">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-        </figure>
-      </div>
-      <div class="media-content">
-        <p class="title is-4">John Smith</p>
-        <p class="subtitle is-6">@johnsmith</p>
-      </div>
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">{{character.name}}</p>
+        </header>
+        <div class="card-image">
+            <figure class="image is-4by3">
+                <img :src="character.image" alt="Placeholder image">
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="content">
+                <b>Status:</b> {{character.status}}<br>
+                <b>Location:</b> {{character.location.name}}<br>
+                <b>Origin:</b> {{character.origin.name}}
+            </div>
+        </div>
     </div>
-
-    <div class="content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-      <a href="#">#css</a> <a href="#">#responsive</a>
-      <br>
-      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-    </div>
-  </div>
-</div>
 </template>
 
 <script>
 export default {
-
+    props: ['character']
 }
 </script>
 
 <style>
-
+    .card {
+        height:100%;
+    }
 </style>
--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -1,7 +1,9 @@
 <template>
+    <simple-pagination @prev="prev()" @next="next()" :info="info"></simple-pagination>
+    <full-pagination :info="info" :current="currentPage" @goToPage="goToPage"></full-pagination>
     <div class="columns is-multiline">
         <div v-for="character in characters" :key="character.id" class="column is-one-quarter">
-            <character-card></character-card>
+            <character-card :character="character"></character-card>
         </div>   
     </div>
 </template>
@@ -9,19 +11,39 @@
 <script>
 import axios from 'axios';
 import CharacterCard from '../components/CharacterCard.vue';
+import SimplePagination from '../components/SimplePagination.vue';
+import FullPagination from '../components/FullPagination.vue';
 export default {
-  components: { CharacterCard },
+  components: { CharacterCard, SimplePagination, FullPagination },
     created(){
-        axios.get('https://rickandmortyapi.com/api/character').then(res => {
-            console.log(res.data);
-            this.info = res.data.info;
-            this.characters = res.data.results;
-        });
+        this.getPage('https://rickandmortyapi.com/api/character');
     },
     data(){
         return {
             info: {},
-            characters: [] 
+            characters: [],
+            currentPage: 1
+        }
+    },
+    methods: {
+        getPage(url){
+            axios.get(url).then(res => {
+                console.log(res.data);
+                this.info = res.data.info;
+                this.characters = res.data.results;
+            });
+        },
+        prev(){
+            this.currentPage--;
+            this.getPage(this.info.prev);
+        },
+        next(){
+            this.currentPage++;
+            this.getPage(this.info.next);
+        },
+        goToPage(page){
+            this.currentPage=page;
+            this.getPage('https://rickandmortyapi.com/api/character?page=' + page);
         }
     }
 }
