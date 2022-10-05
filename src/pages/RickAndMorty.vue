667c5f7e0c043c8d8131cf4c836b1bd9ac66dba8
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-10-05 13:59:38 +0300

    add infinity scroll and search

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -1,4 +1,5 @@
 <template>
+    <input type="text" class="input" v-model="search" @input="filter">
     <simple-pagination @prev="prev()" @next="next()" :info="info"></simple-pagination>
     <full-pagination :info="info" :current="currentPage" @goToPage="goToPage"></full-pagination>
     <div class="columns is-multiline">
@@ -18,11 +19,21 @@ export default {
     created(){
         this.getPage('https://rickandmortyapi.com/api/character');
     },
+    mounted(){
+        // window.addEventListener('scroll', evt => {
+        //     if(window.scrollY+window.innerHeight > document.body.scrollHeight*0.7 && this.hasLoaded){
+        //         this.next();
+        //     }
+        // });
+    },
     data(){
         return {
             info: {},
             characters: [],
-            currentPage: 1
+            currentPage: 1,
+            hasLoaded: true,
+            search: '',
+            searchTimeout: null,
         }
     },
     methods: {
@@ -31,6 +42,7 @@ export default {
                 console.log(res.data);
                 this.info = res.data.info;
                 this.characters = res.data.results;
+                this.hasLoaded = true;
             });
         },
         prev(){
@@ -38,12 +50,26 @@ export default {
             this.getPage(this.info.prev);
         },
         next(){
+            this.hasLoaded=false;
             this.currentPage++;
             this.getPage(this.info.next);
         },
         goToPage(page){
             this.currentPage=page;
-            this.getPage('https://rickandmortyapi.com/api/character?page=' + page);
+            if(this.search){
+                this.getPage('https://rickandmortyapi.com/api/character?page=' + page + '&name=' + this.search);
+            } else {
+                this.getPage('https://rickandmortyapi.com/api/character?page=' + page);
+            }
+        },
+        filter(evt){
+            clearTimeout(this.searchTimeout);
+            this.searchTimeout = setTimeout(() => {
+                this.currentPage = 1;
+                this.getPage('https://rickandmortyapi.com/api/character?name=' + this.search);
+            }, 1000);
+            
+
         }
     }
 }
