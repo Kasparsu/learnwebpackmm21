af1b82ae839d2f394c23f9078bb3b964b5133bbd
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-10-11 11:39:19 +0300

    Add corono api example with search and sort

---
+++ src/components/CountryCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card">
+        <header class="card-header has-background-primary">
+            <p class="card-header-title">{{country.Country}}</p>
+            <figure class="image is-64x64">
+                <img :src="'https://countryflagsapi.com/png/' + country.CountryCode">
+            </figure>
+        </header>
+        <div class="card-content">
+            <div class="content">
+                <b>New Confirmed:</b> {{country.NewConfirmed}}<br>
+                <b>New Deaths:</b> {{country.NewDeaths}}<br>
+                <!-- <b>New Recovered:</b> {{country.NewRecovered}}<br> -->
+                <b>Total Confirmed:</b> {{country.TotalConfirmed}}<br>
+                <b>Total Deaths:</b> {{country.TotalDeaths}}<br>
+                <!-- <b>Total Recovered:</b> {{country.TotalRecovered}}<br> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['country']
+}
+</script>
+
+<style>
+    .card {
+        height:100%;
+    }
+</style>
--- src/index.js
+++ src/index.js
@@ -10,9 +10,11 @@ import Modals from './pages/Modals.vue';
 import Clicker from './pages/Clicker.vue';
 import Chuck from './pages/Chuck.vue';
 import RickAndMorty from './pages/RickAndMorty.vue';
+import Corona from './pages/Corona.vue';
 
 const routes = [
-    { path: '/', component: RickAndMorty, name: 'Rick and Morty' },
+    { path: '/', component: Corona, name: 'Corona' },
+    { path: '/rickandmorty', component: RickAndMorty, name: 'Rick and Morty' },
     { path: '/chuck', component: Chuck, name: 'Chuck' },
     { path: '/clicker', component: Clicker, name: 'Clicker' },
     { path: '/modal', component: Modals, name: 'Modal' },
