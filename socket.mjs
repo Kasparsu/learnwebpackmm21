cc4594795c3ddc2ea33e77120e073d3d90b156df
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-11-01 12:58:11 +0200

    add websocket chat

--- socket.mjs
+++ socket.mjs
@@ -1,11 +1,29 @@
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({port: 8080 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+wss.on('connection', connection);
+
+function connection(ws) {
+  ws.on('message', (data) => {
+    message(data, ws);
   });
+}
 
-  ws.send('something');
-});
+function message(data, ws) {
+  data = JSON.parse(data.toString());
+  types[data.type](data.data, ws);
+  
+}
+let types = {
+  message(data, ws){
+    wss.clients.forEach(function(client) {
+      if(client != ws){
+        client.send(JSON.stringify({type: 'message', data: {text: data, isMe:false, name: ws.name}}));
+      }
+    });
+  },
+  name(data, ws){
+    ws.name = data;
+  }
+}
--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -1,24 +1,67 @@
 <template>
-  
+    <div v-if="!isNameSet">
+        <input type="text" class="input" v-model="name" @keydown.enter="join()" placeholder="Name">
+    </div>
+    <div v-else>  
+        <input type="text" class="input" v-model="message" @keydown.enter="send()" placeholder="Message">
+        <div class="columns" v-for="(msg,index) in messages" :key="index">
+            <div class="column is-half is-flex" :class="{'is-justify-content-end is-offset-half': msg.isMe}">
+                <div class="notification m-2 p-2 is-size-4" :class="{'is-info': !msg.isMe, 'is-primary': msg.isMe}">
+                    {{msg.name}}: <br>
+                    {{msg.text}}
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     mounted(){
-        let ws = new WebSocket('ws://localhost:8080');
-        // Connection opened
-        ws.addEventListener('open', (event) => {
-            ws.send('Hello Server!');
-        });
-
+        this.ws = new WebSocket('ws://localhost:8080');
         // Listen for messages
-        ws.addEventListener('message', (event) => {
-            console.log('Message from server ', event.data);
-        });
+        this.ws.addEventListener('message', this.onMessage);
+    },
+    data(){
+        return {
+            message: '',
+            ws: null,
+            messages: [
+                // {text:'asdasd', isMe: true},
+                // {text:'gdfgfgf', isMe: false},
+                // {text:'vcvcvc', isMe: true},
+            ],
+            isNameSet: false,
+            name: ''
+        }
+    },
+    methods: {
+        sendJson(type, data){
+            this.ws.send(JSON.stringify({type:type, data: data}));
+        },
+        send(){
+           this.sendJson('message', this.message);
+           this.messages.push({text:this.message, isMe: true, name: this.name});
+           this.message = '';
+           
+        },
+        join(){
+            this.sendJson('name', this.name);
+            this.isNameSet = true;
+        },
+        onMessage(event) {
+       
+            let data = JSON.parse(event.data);
+            console.log(data);
+            this.messages.push(data.data);
+        }
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+    .notification {
+        width: fit-content;
+        overflow-wrap: anywhere;
+    }
 </style>
