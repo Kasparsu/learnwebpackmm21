15033e334124c47086fc64446a9bedb9b1b9caff
EerikValguta <90027863+EerikValguta@users.noreply.github.com>
2022-12-07 11:46:50 +0200

    Update Clicker.vue

--- src/pages/Clicker.vue
+++ src/pages/Clicker.vue
@@ -1,63 +1,93 @@
 <template>
-  <div class="columns">
-    <div class="column is-one-third has-text-centered">
-        <b>{{displayCookies}} cookies</b>
-        <br>
-        <b>{{displayCps}} cookies per second</b>
-        <img @click="cookieClick()" :class="{'click': clickClass}" src="https://www.picng.com/upload/cookie/png_cookie_11811.png">
-    </div>
-    <div class="column">
-        <button @click="upgrade(0.1, 10)" class="button is-primary" :disabled="cookies<10">Buy 0.1 cps for 10 cookies</button>
-        <button @click="upgrade(1, 100)" class="button is-primary" :disabled="cookies<100">Buy 1 cps for 100 cookies</button>
-        <button @click="upgrade(10, 1000)" class="button is-primary" :disabled="cookies<1000">Buy 10 cps for 1000 cookies</button>
-        <button @click="upgrade(100, 10000)" class="button is-primary" :disabled="cookies<10000">Buy 100 cps for 10000 cookies</button>
-        <button @click="upgrade(1000, 100000)" class="button is-primary" :disabled="cookies<100000">Buy 1000 cps for 100000 cookies</button>
-    </div>
-  </div>
-</template>
+    <div class="columns">
+        <div class="column is-one-third has-text-centered">
+            <b>{{clicks}} clicks</b>  
+            <br>
+            <b>{{cps}} Diamonds per second</b>  
+            <img @click="clickClick()" :class="{'click': clickClass}" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/be02a4e3-e357-4bde-b048-3a8a59e11d49/d2mcev3-5ac40603-bd13-49ed-b235-859ee4fd46cb.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2JlMDJhNGUzLWUzNTctNGJkZS1iMDQ4LTNhOGE1OWUxMWQ0OVwvZDJtY2V2My01YWM0MDYwMy1iZDEzLTQ5ZWQtYjIzNS04NTllZTRmZDQ2Y2IucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.LJLK_Ka0GIqVx1C0RwYv7vvE4Vgc3BuYUdttCUAJ3RM">   
+        </div>
+        <div class="column">
+            <button @click="upgrade(1, 10)" class="button" :disabled="clicks<10">Buy 1 cps for 10 diamonds</button>     
+            <br>
+            <br>
+            <button @click="upgrade(10, 100)" class="button" :disabled="clicks<100">Buy 10 cps for 100 diamonds</button> 
+            <br>
+            <br>
+            <button @click="upgrade(100, 500)" class="button" :disabled="clicks<500">Buy 100 cps for 500 diamonds</button>
+            <br>  
+            <br>    
+            <button @click="upgrade(1000, 1500)" class="button" :disabled="clicks<1500">Buy 1000 cps for 1500 diamonds</button> 
+            <br>
+            <br>
+            <button @click="reloadPage" class="button" :disabled="clicks<2000">Winner Winner Chicken dinner</button>
 
+        </div>
+    </div>
+</template>
 <script>
+
 export default {
     created(){
         setInterval(()=>{
-            this.cookies += this.cps;
+            this.clicks += this.cps;
+            this.clicks = parseFloat(this.clicks.toFixed(1));
+
+
         }, 1000);
+        
     },
     data(){
         return {
             clickClass: false,
-            cookies: 0,
+            clicks: 0,
             cps: 0
-        }
-    },
-    computed: {
-        displayCookies(){
-            return parseFloat(this.cookies.toFixed(1));
-        },
-        displayCps(){
-            return parseFloat(this.cps.toFixed(1));
+
         }
     },
     methods: {
-        cookieClick(){
-            this.cookies++;
+        clickClick(){
+            this.clicks++;
             this.clickClass = true;
             setTimeout(()=> {
                 this.clickClass = false;
             }, 100);
         },
+        reloadPage() {
+      window.location.reload();
+       },
+
         upgrade(cps, cost){
-            if(this.cookies >= cost){
-                this.cookies -= cost;
+            if(this.clicks >= cost){
+                this.clicks -= cost;
                 this.cps += cps;
             }
         }
     }
+
 }
+
 </script>
 
 <style scoped>
     img.click {
         transform: scale(0.9);
     }
+
+    button {
+    background: #428BCA;
+    color: #fff;
+    font-family: Sans-serif;
+    font-size: 20px;
+    height: 60px;
+    width: 320px;
+    line-height: 60px;
+    margin: 25px 25px;
+    text-align: center;
+    border: 0;
+    transition: all 0.3s ease 0s;
+}
+
+button:hover {
+  background: #e000fd;
+}
 </style>
